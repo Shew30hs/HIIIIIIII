1d804be227dcebdb35033ed77d92037993fb600f
anreh <anurehka@gmail.com>
2018-10-18 11:38:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/hiiiiiiii/HIIIIIi2.java
@@ -0,0 +1,13 @@
+package com.example.taruc.hiiiiiiii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HIIIIIi2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hiiiiii2);
+    }
+}
